4f4ef9500eed1e6d768d8dd746169c2decbe99f3
ameyapanse <ameya.panse@gmail.com>
2023-05-17 03:27:29 -0700

    Wrote embedder

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from langchain.agents import load_tools
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
 from langchain.llms import OpenAI
-import os
+from langchain.document_loaders import UnstructuredMarkdownLoader
+from langchain.vectorstores import Chroma
+from embed import Embedder
+from langchain.embeddings.openai import OpenAIEmbeddings
 
-os.environ['OPENAI_API_KEY'] = "your_openai_api_key"
+import os
+from os import listdir
+from os.path import isfile, join
+
+os.environ['OPENAI_API_KEY'] = "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"
+
+# Embed Docs
+embedder = Embedder()
+embedder.embed_all_docs()
+
+
