b0c9018c28dd1908d996cbb9c8c02aac4b5c3e5a
ameyapanse <ameya.panse@gmail.com>
2023-05-17 04:38:52 -0700

    Base qa

--- main.py
+++ main.py
@@ -10,10 +10,20 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 import os
 from os import listdir
 from os.path import isfile, join
+from langchain.chains.question_answering import load_qa_chain
 
 os.environ['OPENAI_API_KEY'] = "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"
 
 # Embed Docs
 embedder = Embedder()
 embedder.embed_all_docs()
+db = embedder.get_vectorstore()
+
+def single_query(query='What are some milestone model architectures and papers in the last few years?'):
+    relevant_docs = db.similarity_search(query, 5)
+    chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
+    return chain({"input_documents": relevant_docs, "question": query})
+
+
+
 
