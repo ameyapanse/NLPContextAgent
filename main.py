8c00fa56266a1abff57fb77c2348409c37c99a59
ameyapanse <ameya.panse@gmail.com>
2023-05-17 06:40:47 -0700

    Base Conversation

--- main.py
+++ main.py
@@ -26,4 +26,25 @@ def single_query(query='What are some milestone model architectures and papers i
 
 
 
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.agents import initialize_agent
 
+def conversation(q1,q2):
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0), db.as_retriever(), memory=memory)
+    print(conversation_chain({'question': q1})['answer'])
+    print(conversation_chain({'question': q2})['answer'])
+
+
+if __name__ == '__main__':
+    ret = []
+    for q in ['What are some milestone model architectures and papers in the last few years?',
+              "What are the layers in a transformer block?",
+              "Tell me about datasets used to train LLMs and how they’re cleaned"]:
+        ret.append(single_query(q))
+    print(ret)
+    print('---Conversation agent---')
+    q1 = "What are some milestone model architectures and papers in the last few years?"
+    q2 = 'Are there any published by NVIDIA?'
+    conversation(q1,q2)
